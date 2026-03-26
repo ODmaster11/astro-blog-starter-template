5fc2ca2c11653634c079d34a76fbe06ab1a14e7d
ODmaster11 <overdose.webmaster@gmail.com>
2026-03-26 20:48:44 +0700

    update cano

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://tourmovie.info/",
   integrations: [mdx(), sitemap()],
 
   adapter: cloudflare({
